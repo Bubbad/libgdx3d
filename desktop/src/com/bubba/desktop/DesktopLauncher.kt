a48067671042754278d72426a1701970461a5318
Johan Nilsson Hansen <johan.hansen@meltwater.com>
2020-12-29 14:03:34 +0100

    Bumping versions

---
+++ desktop/src/com/bubba/desktop/DesktopLauncher.kt
@@ -0,0 +1,14 @@
+package com.bubba.desktop
+
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.bubba.MyGdxGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(MyGdxGame(), config)
+    }
+}
