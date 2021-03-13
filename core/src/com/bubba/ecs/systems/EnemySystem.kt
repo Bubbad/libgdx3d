821cc6d57371e701ed13c573fe133ae0e960b98a
Johan Nilsson Hansen <johan.hansen@meltwater.com>
2021-03-13 11:41:38 +0100

    Enemy now looks at the player

--- core/src/com/bubba/ecs/systems/EnemySystem.kt
+++ core/src/com/bubba/ecs/systems/EnemySystem.kt
@@ -6,18 +6,25 @@ import com.badlogic.ashley.core.EntityListener
 import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.ashley.core.Family
 import com.badlogic.gdx.math.Matrix4
+import com.badlogic.gdx.math.Quaternion
 import com.badlogic.gdx.math.Vector3
 import com.bubba.ecs.components.CharacterMoveComponent
 import com.bubba.ecs.components.EnemyComponent
 import com.bubba.ecs.components.ModelComponent
+import com.bubba.ecs.components.PlayerComponent
 import ktx.ashley.get
+import kotlin.math.atan2
 
 class EnemySystem: EntitySystem(), EntityListener {
 
     private val entities = mutableListOf<Entity>()
 
     private val ghostMatrix4 = Matrix4()
-    private val enemyTranslation = Vector3()
+    private val enemyGhostTranslation = Vector3()
+
+    private val rotationBetweenEnemyAndPlayer = Quaternion()
+    private val enemyModelTranslation = Vector3()
+    private val playerModelTranslation = Vector3()
 
     override fun addedToEngine(engine: Engine) {
         engine.addEntityListener(
@@ -28,15 +35,46 @@ class EnemySystem: EntitySystem(), EntityListener {
     }
 
     override fun update(deltaTime: Float) {
+
+        val playerEntity = engine.getEntitiesFor(
+            Family.all(
+                PlayerComponent::class.java,
+                ModelComponent::class.java
+            ).get()
+        ) ?: return
+
         entities.forEach { entity ->
             val characterMoveComponent = entity[CharacterMoveComponent.mapper]!!
-            val modelComponent = entity[ModelComponent.mapper]!!
+            val enemyModelComponent = entity[ModelComponent.mapper]!!
 
             characterMoveComponent.ghostObject.getWorldTransform(ghostMatrix4)
-            ghostMatrix4.getTranslation(enemyTranslation)
+            ghostMatrix4.getTranslation(enemyGhostTranslation)
 
-            modelComponent.modelInstance.transform.set(enemyTranslation.x, enemyTranslation.y, enemyTranslation.z, 0f, 0f, 0f, 0f)
+            val playerModel = playerEntity[0][ModelComponent.mapper]!!
+            val quaternion = getRotationBetweenEnemyAndPlayer(playerModel, enemyModelComponent)
+
+            enemyModelComponent.modelInstance.transform.set(
+                enemyGhostTranslation.x,
+                enemyGhostTranslation.y,
+                enemyGhostTranslation.z,
+                quaternion.x,
+                quaternion.y,
+                quaternion.z,
+                quaternion.w
+            )
         }
+    }
+
+    private fun getRotationBetweenEnemyAndPlayer(playerModel: ModelComponent, enemyModel: ModelComponent): Quaternion {
+        playerModel.modelInstance.transform.getTranslation(playerModelTranslation)
+        enemyModel.modelInstance.transform.getTranslation(enemyModelTranslation)
+
+        val deltaX = playerModelTranslation.x - enemyModelTranslation.x
+        val deltaZ = playerModelTranslation.z - enemyModelTranslation.z
+
+        val angle = atan2(deltaX.toDouble(), deltaZ.toDouble())
+
+        return rotationBetweenEnemyAndPlayer.setFromAxis(Vector3.Y, (Math.toDegrees(angle) + 90).toFloat())
     }
 
     override fun entityAdded(entity: Entity) {
